88d87b30c4539ec9067b271dcf8f67e8508dd071
kosmonaft3010 <claud055876@gmail.com>
2022-11-22 22:36:42 +0300

    adedd dz_4

--- dz_4/Program.cs
+++ dz_4/Program.cs
@@ -4,17 +4,13 @@
 // 8 -> 2, 4, 6, 8
 
 Console.Clear();
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число N =");
 int N = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-while ((count) <= N) {
-    if ((count % 2) == 0)
-    {
-       Console.Write("{0}", count);
-       count = count + 1;
-    }
-    else
-    {
-       count = count + 1;
-    }
+
+int count = 2;
+
+while (count<=N)
+{
+Console.WriteLine(count);
+count+=2;
 }
